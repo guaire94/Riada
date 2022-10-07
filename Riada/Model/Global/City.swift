d59213e60de856b2ca77d34060067948ab673b2e
Quentin Gallois <guaire94.perso@gmail.com>
2022-10-07 10:05:50 +0400

    no message

--- Riada/Model/Global/City.swift
+++ Riada/Model/Global/City.swift
@@ -23,60 +23,51 @@ struct City {
 }
 
 enum PlaceHolderCity: CaseIterable {
-    case paris
-//    case dubai
-//    case abuDhabi
-//    case sharjah
+    case dubai
+    case abuDhabi
+    case sharjah
 
     var name: String {
         switch self {
-        case .paris:
-            return "Paris"
-//        case .dubai:
-//            return "Dubai"
-//        case .abuDhabi:
-//            return "Abu Dhabi"
-//        case .sharjah:
-//            return "Sharjah"
+        case .dubai:
+            return "Dubai"
+        case .abuDhabi:
+            return "Abu Dhabi"
+        case .sharjah:
+            return "Sharjah"
         }
     }
     
     var placeId: String {
         switch self {
-        case .paris:
-            return "ChIJD7fiBh9u5kcRYJSMaMOCCwQ"
-//        case .dubai:
-//            return "ChIJRcbZaklDXz4RYlEphFBu5r0"
-//        case .abuDhabi:
-//            return "ChIJufI-cg9EXj4RCBGXQZMuzMc"
-//        case .sharjah:
-//            return "ChIJS5bn7V9fXz4RiW0fnKEKgyo"
+        case .dubai:
+            return "ChIJRcbZaklDXz4RYlEphFBu5r0"
+        case .abuDhabi:
+            return "ChIJufI-cg9EXj4RCBGXQZMuzMc"
+        case .sharjah:
+            return "ChIJS5bn7V9fXz4RiW0fnKEKgyo"
         }
     }
     
     var lat: Double {
         switch self {
-        case .paris:
-            return 48.856614
-//        case .dubai:
-//            return 25.276987
-//        case .abuDhabi:
-//            return 24.466667
-//        case .sharjah:
-//            return 25.348766
+        case .dubai:
+            return 25.276987
+        case .abuDhabi:
+            return 24.466667
+        case .sharjah:
+            return 25.348766
         }
     }
     
     var lng: Double {
         switch self {
-        case .paris:
-            return 2.3522219
-//        case .dubai:
-//            return 55.296249
-//        case .abuDhabi:
-//            return 54.366669
-//        case .sharjah:
-//            return 55.405403
+        case .dubai:
+            return 55.296249
+        case .abuDhabi:
+            return 54.366669
+        case .sharjah:
+            return 55.405403
         }
     }
     
